4d482781fa191bbccd99cf6c957c472bdba25e47
Hardik Naik <96820742+mehardiknaik@users.noreply.github.com>
2023-03-09 15:14:40 +0530

    Update page.tsx

--- src/app/[type]/page.tsx
+++ src/app/[type]/page.tsx
@@ -5,5 +5,5 @@ import React from "react";
 export default async function Pages({ params: { type } }: any) {
   // Initiate both requests in parallel
 
-  return <div>{type}</div>;
+  return <div>{type} Coming Soon.....</div>;
 }
